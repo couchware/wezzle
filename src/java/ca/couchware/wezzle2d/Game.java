e6263678dc4e2d754e7cea9e92da93091449f9ca
cam <cam@93abb39b-6d47-0410-9697-f70035e0d922>
2010-01-15 04:25:19 +0000

    Wezzle 1.0 muthafuckaaaaaaaaas!

--- src/java/ca/couchware/wezzle2d/Game.java
+++ src/java/ca/couchware/wezzle2d/Game.java
@@ -96,7 +96,7 @@ public class Game extends Canvas implements IWindowCallback
     final public static String APPLICATION_NAME = "Wezzle";
     
     /** The version of the application. */
-    final public static String APPLICATION_VERSION = "Beta 2";
+    final public static String APPLICATION_VERSION = "1.0";
     
     /** The full title of the game. */
     final public static String TITLE = APPLICATION_NAME + " " + APPLICATION_VERSION;
