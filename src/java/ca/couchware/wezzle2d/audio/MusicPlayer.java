70fc9bdb3b43ce5910ba3e5701dda9930495ffb8
cam <cam@93abb39b-6d47-0410-9697-f70035e0d922>
2010-03-22 02:35:53 +0000

    Added code to make the fader safer.

--- src/java/ca/couchware/wezzle2d/audio/MusicPlayer.java
+++ src/java/ca/couchware/wezzle2d/audio/MusicPlayer.java
@@ -9,6 +9,7 @@ import ca.couchware.wezzle2d.util.CouchLogger;
 import ca.couchware.wezzle2d.util.AtomicDouble;
 import ca.couchware.wezzle2d.util.NumUtil;
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
 import paulscode.sound.SoundSystemConfig;
 import paulscode.sound.SoundSystemJPCT;
 
@@ -22,7 +23,7 @@ public class MusicPlayer
 {
     private static final double MIN_GAIN = 0.0;
     private static final double MAX_GAIN = 1.0;
-    private static final double EPSILON = 0.01;
+    private static final double EPSILON = 0.001;
     private static final double FADE_DELTA = 0.02;
     private static final int FADE_PERIOD = 50;
     private static final int STOP_PERIOD = 250;
@@ -35,6 +36,11 @@ public class MusicPlayer
     final private Object playerLock = new Object();
     private AtomicDouble normalizedGain = new AtomicDouble();
     private AtomicBoolean loop = new AtomicBoolean(false);
+    
+    /* The fade direction is 1 for up, -1 for down */
+    final private static int DIR_UP = 1;
+    final private static int DIR_DOWN = -1;
+    private AtomicInteger fadeDirection = new AtomicInteger(0);
 
     /**
      * The constructor.
@@ -159,7 +165,7 @@ public class MusicPlayer
      * @param targetGain The target gain to fade to, from 0.0 to 1.0.    
      */
     public void fadeToGain(final double nGain)
-    {        
+    {
         if (fadeThread != null && fadeThread.isAlive())
         {
             try
@@ -174,29 +180,32 @@ public class MusicPlayer
 
         fadeThread = new Thread()
         {
-            private double targetNormalizedGain = nGain;
+            final private double targetNormalizedGain = nGain;
 
             @Override
             public void run()
             {
                 cancelFade.set(false);
+                
+                if (nGain > normalizedGain.get())
+                    fadeDirection.set(DIR_UP);
+                else
+                    fadeDirection.set(DIR_DOWN);
 
                 while (true)
                 {
+                    final int dir = fadeDirection.get();
                     final double n = normalizedGain.get();
-                    double delta = FADE_DELTA;
+                    final double delta = FADE_DELTA * dir;
 
-                    if (cancelFade.get()
-                            || NumUtil.equalsDouble(n, targetNormalizedGain, EPSILON))
+                    final boolean atUpper = dir == DIR_UP && n >= targetNormalizedGain;
+                    final boolean atLower = dir == DIR_DOWN && n <= targetNormalizedGain;
+
+                    if (cancelFade.get() || atUpper || atLower)
                     {
                         // End execution.
                         break;
-                    }
-
-                    if (n > targetNormalizedGain)
-                    {
-                        delta *= -1;
-                    }
+                    }                    
 
                     final double g = n + delta;
                     setNormalizedGain(g);
@@ -258,20 +267,26 @@ public class MusicPlayer
     {
         stopThread = new Thread()
         {
-            final private double targetGain = nGain;
+            final private double targetNormalizedGain = nGain;
 
             @Override
             public void run()
             {
                 cancelStop.set(false);
 
+                if (nGain > normalizedGain.get())
+                    fadeDirection.set(DIR_UP);
+                else
+                    fadeDirection.set(DIR_DOWN);
+
                 while (true)
                 {
+                    final int dir = fadeDirection.get();
                     final double n = normalizedGain.get();
+                    final boolean atUpper = dir == DIR_UP && n >= targetNormalizedGain;
+                    final boolean atLower = dir == DIR_DOWN && n <= targetNormalizedGain;
 
-                    if (NumUtil.equalsDouble(targetGain, n, EPSILON)
-                            || cancelStop.get()
-                            || isFinished())
+                    if (atUpper || atLower || cancelStop.get() || isFinished())
                     {
                         synchronized (playerLock)
                         {
